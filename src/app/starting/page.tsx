a67f7577e27742bf3df5e94b69e167ca3facb9aa
saemii-24 <hims2419@gmail.com>
2024-07-02 18:33:00 +0900

    Test: drag box 연습

--- src/app/starting/page.tsx
+++ src/app/starting/page.tsx
@@ -23,12 +23,65 @@ export default function Home() {
     console.log(pokemonRefs);
   }, []);
 
+  let [arr, setArr] = useState<string[]>([
+    "피카츄1",
+    "라이츄2",
+    "파이리3",
+    "꼬북이4",
+  ]);
+
+  let [dragItem, setDragItem] = useState<HTMLDivElement | null>(null);
+  let [dropItem, setDropItem] = useState<HTMLDivElement | null>(null);
+  let [itemNum, setItemNum] = useState<number | null>(null);
+  const handleDrag = (e: any) => {
+    //어떤 아이템위로 드래그 되는가?
+    const targetItem = e.target;
+    console.log(targetItem);
+    console.log("현재 어떤 아이템 위로 드래그 시도중인가?: " + targetItem);
+    setDropItem(targetItem);
+  };
+  const nowDragItem = (e: any) => {
+    const targetItem = e.target;
+    // console.log(targetItem);
+    console.log("현재 드래그 중인 아이템: " + targetItem);
+    setDragItem(targetItem);
+  };
+  const dropNow = (e: any) => {
+    const targetItem = e.target;
+    console.log(dragItem, dropItem);
+
+    //현재 놓아질 자리의 인덱스 찾기
+    const filterIndex = arr.findIndex((item, index) => {
+      if (dropItem?.innerText === item) {
+        console.log(dropItem.innerText);
+        return true;
+      }
+    });
+    console.log(filterIndex);
+
+    //현재 드래그하는 박스의 인덱스 찾기
+    const dragIndex = arr.findIndex((item, index) => {
+      if (dragItem?.innerText === item) {
+        console.log(dragItem.innerText);
+        return true;
+      }
+    });
+    console.log(dragIndex);
+
+    //arr 순서 바꾸기
+    let newArr = [...arr];
+    newArr.splice(dragIndex, 1);
+    newArr.splice(filterIndex, 0, arr[dragIndex]);
+    console.log(newArr);
+    setArr(newArr);
+  };
+
   return (
     <>
       <Header text={header.start} />
       <main className="mt-20 flex justify-center items-center">
         <div className="container grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
-          {arr.map((item, i) => {
+          {/* {arr.map((item, i) => {
             return (
               <div
                 key={i}
@@ -38,6 +91,26 @@ export default function Home() {
                 onClick={handleClick}
               >
                 <Pokemon num={i + 1} />
+              </div>
+            );
+          })} */}
+          {arr.map((item, i) => {
+            return (
+              <div
+                key={i}
+                draggable
+                className={`bg-red-100 box-${i + 1} h-[300px]`}
+                onDragEnter={(e) => {
+                  handleDrag(e);
+                }}
+                onDragStart={(e) => {
+                  nowDragItem(e);
+                }}
+                onDragEnd={(e) => {
+                  dropNow(e);
+                }}
+              >
+                {item}
               </div>
             );
           })}
