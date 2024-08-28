4efb557034fc6e6cef59ed17ea00c8a0a6a2e3ec
saemii-24 <hims2419@gmail.com>
2024-08-28 22:46:08 +0900

    style:chart.js

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,8 +1,12 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import { Chart, registerables } from "chart.js";
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, registerables, defaults } from "chart.js";
 
+// 필요한 요소 등록
 Chart.register(...registerables);
+
+// defaults.aspectRatio = true;
 
 export function toRadians(degrees) {
   return degrees * (Math.PI / 180);
@@ -16,7 +20,8 @@ const segments = [
   },
   {
     title: "Segment 2",
-    color1: "#118ab2",
+    color1: "#06d6a0",
+    color2: "#118ab2",
   },
   {
     title: "Segment 3",
@@ -26,80 +31,85 @@ const segments = [
   {
     title: "Segment 4",
     color1: "#06d6a0",
-    color2: null,
+    color2: "#ef476f",
   },
   {
     title: "Segment 5",
     color1: "#ef476f",
-    color2: null,
+    color2: "#118ab2",
   },
 ];
 
-function createConicGradient(ctx, c1, c2, startAngle) {
-  const centerX = ctx.canvas.width / 2;
-  const centerY = ctx.canvas.height / 2;
+function createConicGradient(context, c1, c2) {
+  const { dataIndex, chart, dataset } = context;
+  const { chartArea, ctx } = chart;
+
+  if (!chartArea || !ctx) {
+    return null;
+  }
+
+  const countSegments = dataset.data.length;
+  const segmentSizeOfDeg = 360 / countSegments;
+  const rotation = dataIndex * segmentSizeOfDeg - 90;
+  const startAngle = toRadians(rotation);
+
+  const centerX = (chartArea.left + chartArea.right) / 2;
+  const centerY = (chartArea.top + chartArea.bottom) / 2;
+
   const conicGradient = ctx.createConicGradient(startAngle, centerX, centerY);
   conicGradient.addColorStop(0, c1);
-  conicGradient.addColorStop(1, c2 ?? c1);
+  conicGradient.addColorStop(1 / countSegments, c2 ?? c1);
   return conicGradient;
 }
 
-const DoughnutChart = () => {
-  const chartRef = useRef(null);
+const data = segments.map(() => 1);
+const labels = segments.map(({ title }) => title);
 
-  useEffect(() => {
-    const ctx = chartRef.current.getContext("2d");
-    const data = segments.map(() => 1);
-    const labels = segments.map(({ title }) => title);
+const getBackgroundColors = (ctx) =>
+  segments.map(({ color1, color2 }) =>
+    createConicGradient(ctx, color1, color2)
+  );
 
-    const getBackgroundColors = () =>
-      segments.map(({ color1, color2 }, index) => {
-        const segmentSizeOfDeg = 360 / segments.length;
-        const startAngle = toRadians(index * segmentSizeOfDeg - 90);
-        return createConicGradient(ctx, color1, color2, startAngle);
-      });
+const dataChart = {
+  datasets: [
+    {
+      label: "My First Dataset",
+      labels,
+      data,
+      backgroundColor: getBackgroundColors,
+      hoverOffset: 10,
+    },
+  ],
+};
 
-    const dataChart = {
-      labels: labels,
-      datasets: [
-        {
-          label: "My First Dataset",
-          data: data,
-          backgroundColor: getBackgroundColors(),
-          hoverOffset: 10,
-        },
-      ],
-    };
-
-    const config = {
-      type: "doughnut",
-      data: dataChart,
-      options: {
-        responsive: true,
-        animation: false,
-        plugins: {
-          tooltip: {
-            yAlign: "bottom",
-            displayColors: false,
-            callbacks: {
-              label: function (context) {
-                const { dataIndex, dataset } = context;
-                return dataset.labels[dataIndex];
-              },
-            },
+const config = {
+  type: "doughnut",
+  data: dataChart,
+  options: {
+    responsive: true,
+    animation: false,
+    plugins: {
+      tooltip: {
+        yAlign: "bottom",
+        displayColors: false,
+        callbacks: {
+          label: function (context) {
+            const { dataIndex, dataset } = context;
+            let label = dataset.labels[dataIndex];
+            return label;
           },
         },
       },
-    };
-
-    const chart = new Chart(ctx, config);
-
-    return () => {
-      chart.destroy();
-    };
-  }, []);
-
-  return <canvas ref={chartRef} />;
+    },
+  },
 };
 
-export default DoughnutChart;
+const GradientDoughnut = () => {
+  return (
+    <div>
+      <Doughnut data={dataChart} options={config.options} />
+    </div>
+  );
+};
+
+export default GradientDoughnut;
